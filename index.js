86a3787468dfc85a4b2c8f58522123e5b1d1535a
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-04-27 12:53:14 +0600

    get data by email and show my art craft list

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
         res.send(result)
       })
 
+    app.get('/mylist/:email', async (req, res) => {
+        const email = req.params.email
+        const result = await craftCollection.find({email}).toArray()
+        res.send(result)
+
+    })  
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
