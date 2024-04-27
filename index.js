9f69b6773d361933a071fdab57652c5e93c36a67
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-04-27 15:10:19 +0600

    delete oparation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -42,6 +42,13 @@ async function run() {
         res.send(result)
 
     })  
+
+    app.delete('/mylist/:id', async (req,res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
